2f5e9289d77d612925a570e2e262a7067bdd25dc
bakytkhamraev <khamraevbakyt@gmail.com>
2019-12-08 19:11:55 +0600

    final

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,7 +8,6 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         ArrayList<String> listA = new ArrayList<>(5);
-        System.out.println("Список А");
         listA.add(sc.nextLine());
         listA.add(sc.nextLine());
         listA.add(sc.nextLine());
@@ -18,17 +17,20 @@ public class Main {
         Iterator<String> iter = listA.iterator();
         while (iter.hasNext()) {
             String a = iter.next();
-
             System.out.println(listA);
+
+        System.out.println("Список А");
+
 
             listA.ensureCapacity(10);
             ArrayList<String> listB = new ArrayList<>(10);
+
+            listB.add(sc.nextLine());
+            listB.add(sc.nextLine());
+            listB.add(sc.nextLine());
+            listB.add(sc.nextLine());
+            listB.add(sc.nextLine());
             System.out.println("Список B");
-            listB.add(sc.nextLine());
-            listB.add(sc.nextLine());
-            listB.add(sc.nextLine());
-            listB.add(sc.nextLine());
-            listB.add(sc.nextLine());
 
             iter = listB.iterator();
             while (iter.hasNext()) {
@@ -38,6 +40,13 @@ public class Main {
 
 
             ArrayList<String> listC = new ArrayList<>();
+
+            listC.add(sc.nextLine());
+            listC.add(sc.nextLine());
+            listC.add(sc.nextLine());
+            listC.add(sc.nextLine());
+            listC.add(sc.nextLine());
+
             Collections.sort(listA);
             Collections.sort(listB);
             Collections.reverse(listB);
@@ -48,6 +57,14 @@ public class Main {
 
             Collections.sort(listC);
             listC.toString().length();
+
+
+            Iterator<String> iterator = listC.iterator();
+            while (iter.hasNext()) {
+                String b = iter.next();
+
+            }
+
 
             System.out.println(listC);
         }
